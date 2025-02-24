d103a12257c6ec986d3e16a6a86ee98e689edecc
Надежда Шенфельд <fumuse.shore@gmail.com>
2025-02-24 16:11:31 +0300

    fix: исправление редактирования ассет-объектов

--- Assets/_Gamebox25_Prehistoric/Scripts/ScriptableObjects/Configs/GameplayConfig.cs
+++ Assets/_Gamebox25_Prehistoric/Scripts/ScriptableObjects/Configs/GameplayConfig.cs
@@ -3,6 +3,6 @@
 [CreateAssetMenu(fileName = "GameplayConfig", menuName = "Configs/GameplayConfig", order = 1100)]
 public class GameplayConfig : ScriptableObject
 {
-    [field: SerializeField] public float ParticleCauldronSpawnRadius { get; } = 2f;
-    [field: SerializeField] public float ParticleFollowSpeed { get; } = 10f;
+    [field: SerializeField] public float ParticleCauldronSpawnRadius { get; private set; } = 2f;
+    [field: SerializeField] public float ParticleFollowSpeed { get; private set; } = 10f;
 }
--- Assets/_Gamebox25_Prehistoric/Scripts/ScriptableObjects/Configs/PlayerConfig.cs
+++ Assets/_Gamebox25_Prehistoric/Scripts/ScriptableObjects/Configs/PlayerConfig.cs
@@ -3,9 +3,9 @@
 [CreateAssetMenu(fileName = "PlayerConfig", menuName = "Configs/PlayerConfig", order = 1100)]
 public class PlayerConfig : ScriptableObject
 {
-    [field: SerializeField] public float Speed { get; } = 5f;  
+    [field: SerializeField] public float Speed { get; private set; } = 5f;  
     
-    [field: SerializeField] public float JumpForce { get; } = 6f;  
+    [field: SerializeField] public float JumpForce { get; private set; } = 6f;  
     
-    [field: SerializeField] public float TimeToStop { get; } = 5f;  
+    [field: SerializeField] public float TimeToStop { get; private set; } = 5f;  
 }
